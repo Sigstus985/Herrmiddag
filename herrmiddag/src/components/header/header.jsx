e1e7383b693e79ba80616b7815720b1baf71a114
Sigstus985 <siggeluren@gmail.com>
2025-02-04 22:25:38 +0100

    set up react

---
+++ herrmiddag/src/components/header/header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return(
+        <nav>
+            <h1>Herrmiddag</h1>
+
+            <ul>
+                <li></li>
+                <li>Meny</li>
+                <li>Schema</li>
+            </ul>
+        </nav>
+        
+    )    
+}
